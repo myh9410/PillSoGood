1d30a19b21377323db2f7a9d99df12f0c6bf7856
YongHo <mjskks94@naver.com>
2020-09-22 11:24:19 +0900

    :hammer: | update code |

--- frontend/src/page/user/Login.vue
+++ frontend/src/page/user/Login.vue
@@ -1,41 +1,57 @@
 <template>
   <div class="user" id="login" style="background-color:#f2f2f2">
-    <div style="margin:auto; width:50%">
-      <div class="input-wrap email-wrap">
-        <label for="email">이메일</label>
-        <input
-          v-model="email"
-          v-bind:class="{error : error.email, complete: !error.email&&email.length!=0}"
-          @keyup.enter="onLogin"
-          id="email"
-          placeholder="이메일을 입력해주세요"
-          style="height:50px; background-color:white;"
-          type="text"
-        />
-        <div class="error-text" v-if="error.email">{{error.email}}</div>
+    <div class="container" style="margin:auto;">
+      <div class="row">
+        <div class="vl">
+          <span class="vl-innertext">or</span>
+        </div>
+        <div class="col">
+          <div class="input-wrap email-wrap">
+            <label for="email">이메일</label>
+            <input
+              v-model="email"
+              v-bind:class="{error : error.email, complete: !error.email&&email.length!=0}"
+              @keyup.enter="onLogin"
+              id="email"
+              placeholder="이메일을 입력해주세요"
+              style="height:50px; background-color:white;"
+              type="text"
+            />
+            <div class="error-text" v-if="error.email">{{error.email}}</div>
+          </div>
+          <div class="input-wrap password-wrap">
+            <label for="password">비밀번호</label>
+            <input
+              v-model="password"
+              id="password"
+              :type="passwordType"
+              v-bind:class="{error : error.password, complete: !error.password&&password.length!=0}"
+              @keyup.enter="onLogin"
+              placeholder="최소 8자 이상으로 입력해주세요"
+              style="height:50px; background-color:white;"
+            />
+            <span :class="{active : passwordType==='text'}">
+              <i class="fas fa-eye"></i>
+            </span>
+            <div class="error-text" v-if="error.password">{{error.password}}</div>
+          </div>
+          <div class="submit">
+            <input type="submit" value="Login" v-bind:style="loginButton" @click="onLogin" :disabled="!isSubmit" :class="{disabled : !isSubmit}">
+          </div>
+        </div>
+        <div class="col">
+          <div class="snsLogin">
+            <a href="#" class="google btn">
+              Login with Google+
+            </a>
+            <a href="#" class="kakao btn">
+              Login with Kakao
+            </a>
+          </div>
+        </div>
       </div>
-      <div class="input-wrap password-wrap">
-        <label for="password">비밀번호</label>
-        <input
-          v-model="password"
-          id="password"
-          :type="passwordType"
-          v-bind:class="{error : error.password, complete: !error.password&&password.length!=0}"
-          @keyup.enter="onLogin"
-          placeholder="최소 8자 이상으로 입력해주세요"
-          style="height:50px; background-color:white;"
-        />
-        <span :class="{active : passwordType==='text'}">
-          <i class="fas fa-eye"></i>
-        </span>
-        <div class="error-text" v-if="error.password">{{error.password}}</div>
-      </div>
-
-      <GoogleLogin :component="component" />
-
-      <div class="submit">
-        <v-btn large color="#BCAAA4" @click="onLogin" :disabled="!isSubmit" :class="{disabled : !isSubmit}">로그인</v-btn>
-      </div>
+      <br>
+      <br>
     </div>
   </div>
 </template>
@@ -45,12 +61,11 @@ import '../../assets/css/login.scss'
 import PV from "password-validator";
 import * as EmailValidator from "email-validator";
 import http from  "@/util/http-common"
-import GoogleLogin from "../../components/accounts/Google.vue";
+import store from "@/store.js"
 
 export default {
   name: "join",
   components: {
-    GoogleLogin
   },
   data: () => {
     return {
@@ -63,7 +78,13 @@ export default {
         password : false
       },
       isSubmit: false,
-      component: this
+      component: this,
+      loginButton : {
+          backgroundColor: '#4CAF50',
+          color: 'white',
+          width: '80%',
+          opacity: '0.4'
+      }
     };
   },
   created() {
@@ -87,6 +108,10 @@ export default {
     password: function() {
       this.pwdCheckForm();
     },
+    isSubmit: function() {
+      if (this.isSubmit) this.loginButton.opacity="1.0";
+      else this.loginButton.opacity="0.4";
+    }
   },
   methods: {
     emailCheckForm() {
@@ -104,7 +129,9 @@ export default {
 
       let isSubmit = true;
       Object.values(this.error).map(v => {
-        if (v) isSubmit = false;
+        if (v) {
+          isSubmit = false;
+        }
       });
       this.isSubmit = isSubmit;
     },
@@ -116,10 +143,12 @@ export default {
         })
       .then( res => {
         //store에 저장하고 가져다가 씀
-        console.log(res.data.user);
+        store.dispatch('login',res.data.user);
         alert("로그인에 성공하였습니다!");
         this.$router.push("/");
       }). catch( err => {
+        alert("로그인에 실패하였습니다! 다시 시도해주세요");
+        this.$router.push("/user/login");
         console.log(err);
       })
     }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,28 +1,50 @@
-// import Vue from 'vue';
-// import Vuex from 'vuex';
+import Vue from 'vue';
+import Vuex from 'vuex';
 // import router from './router/index.js'
+import createPersistedState from 'vuex-persistedstate'
 
-// Vue.use(Vuex);
+Vue.use(Vuex);
 
+export default new Vuex.Store({
+    plugins: [createPersistedState()],
+    state: {
+        token: '',
+        role:'',
+        isLogged: false,
+        userData:{
+            username:'',
+            password:'',
+            gender:'',
+            birthday:'',
+            email:'',
+            name:'',            
+        },
+        userInfo:{}
+    },
+    mutations:{
+        mutateIsLogin(state, isLogin){
+            state.isLogged = isLogin;
+        },
+        mutateUserInfo(state, userInfo){
+            state.userInfo = userInfo;
+        }
+    },
 
-// export const store = new Vuex.Store({
-//     plugins: [createPersistedState()],
-
-//     state: {
-        
-//         token: '',
-//         role:'',
-//         isLogged: false,
-//         userData:{
-//             username:'',
-//             password:'',
-//             gender:'',
-//             birthday:'',
-//             email:'',
-//             name:'',
-            
-//         },
-       
-//     },
-
-// })
+    getters: {
+        userInfo(state){
+            return state.userInfo;
+        }
+    },
+    actions: {
+        login(context, user) {
+            console.log(user);
+            context.commit('mutateIsLogin', true);
+            context.commit('mutateUserInfo', user);
+        },
+        logout(context){
+            context.commit('mutateIsLogin',false);
+            context.commit('mutateUserInfo',{});
+            alert("로그아웃 되었습니다.");
+        }
+    }
+})
