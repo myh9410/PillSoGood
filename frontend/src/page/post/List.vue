2fd2ff8ded2820965e7a4ead3f3d9e4640c9cfbe
배민규 <bmk0110@naver.com>
2020-09-24 17:30:12 +0900

    :new: | review list & create | S03P23A506-15

--- frontend/src/page/post/List.vue
+++ frontend/src/page/post/List.vue
@@ -22,75 +22,115 @@
           ></v-text-field>
         </div>
         <v-row>
-          <v-col v-for="(sampletonic, i) in sampletonics" :key="i" cols="6" md="3" :search="search">
-            <v-card
-              @click="goDetail(sampletonic)"
-              style="width:200px; height:250px;"
-              :search="search"
-            >
+          <v-col v-for="(tonic, i) in tonics" :key="i" cols="6" md="3" :search="search">
+            <v-card @click="goDetail(tonic)" style="width:200px; height:250px;" :search="search">
               <!-- <div "> -->
               <v-img
-                :src="sampletonic.src"
+                src="../../assets/images/sample1.png"
                 style="width:200px; height:200px; margin:auto;"
                 gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
               ></v-img>
               <!-- </div> -->
               <hr />
-              <h4 style="text-align:center">{{sampletonic.name}}</h4>
+              <h4 style="text-align:center">{{tonic.name}}</h4>
             </v-card>
           </v-col>
         </v-row>
       </div>
     </section>
+
+    <div class="btn-cover d-flex justify-content-center align-items-center">
+      <button :disabled="pageNum === 0" @click="prevPage" class="page-btn btn btn-info m-2">
+        <p>이전</p>
+      </button>
+      <span class="page-count m-0">
+        <h4>{{ pageNum + 1 }} / 86</h4>
+      </span>
+      <button :disabled="pageNum >= 86" @click="nextPage" class="page-btn btn btn-info m-2">
+        <p>다음</p>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-import simage1 from "../../assets/images/sample1.png";
-import simage2 from "../../assets/images/sample2.jpg";
+// import simage1 from "../../assets/images/sample1.png";
+// import simage2 from "../../assets/images/sample2.jpg";
+const API_BASE_URL = "http://localhost:8000";
 export default {
+  components: {
+    // simage1
+  },
   data: () => ({
     search: "",
     channel_name: "",
     tonics: [],
-    sampletonics: [
-      {
-        id: 1,
-        name: "아이키커",
-        src: simage1,
-      },
-      {
-        id: 2,
-        name: "솔가 비타민 B",
-        src: simage2,
-      },
-      {
-        id: 3,
-        name: "솔가 비타민 C",
-        src: simage2,
-      },
-      {
-        id: 4,
-        name: "솔가 비타민 D",
-        src: simage2,
-      },
-      {
-        id: 5,
-        name: "솔가 비타민 E",
-        src: simage2,
-      },
-    ],
+    pageNum: 0,
+
+    // sampletonics: [
+    //   {
+    //     id: 1,
+    //     name: "아이키커",
+    //     src: simage1,
+    //   },
+    //   {
+    //     id: 2,
+    //     name: "솔가 비타민 B",
+    //     src: simage2,
+    //   },
+    //   {
+    //     id: 3,
+    //     name: "솔가 비타민 C",
+    //     src: simage2,
+    //   },
+    //   {
+    //     id: 4,
+    //     name: "솔가 비타민 D",
+    //     src: simage2,
+    //   },
+    //   {
+    //     id: 5,
+    //     name: "솔가 비타민 E",
+    //     src: simage2,
+    //   },
+    // ],
     howto: true,
   }),
+  created() {
+    this.fetchTonicList(1);
+  },
   methods: {
-    goDetail(sampletonic) {
-      this.$router.push(`/list/${sampletonic.id}/`);
+    goDetail(tonic) {
+      this.$router.push(`/list/${tonic.id}/`);
     },
     showAlbum() {
       this.howto = true;
     },
     showList() {
       this.howto = false;
+    },
+    fetchTonicList(page) {
+      const API_TONIC_LIST_URL = API_BASE_URL + `/supplements/list/${page}`;
+      const config = {};
+      this.$axios
+        .get(API_TONIC_LIST_URL, config)
+        .then((res) => {
+          console.log(res.data);
+          this.tonics = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    nextPage() {
+      this.pageNum += 1;
+
+      this.fetchTonicList(this.pageNum);
+    },
+    prevPage() {
+      this.pageNum -= 1;
+
+      this.fetchTonicList(this.pageNum);
     },
   },
 };
--- frontend/src/page/post/TonicDetail.vue
+++ frontend/src/page/post/TonicDetail.vue
@@ -1,53 +1,104 @@
 <template>
   <div>
-    
     <section
       style="width: 100vw;
             height: 100vh;
             padding: 10px;
-            margin:0px;
+            margin:0 auto;
             "
-      class="white--text d-flex"
     >
-      <div
-        class="d-flex"
-        style="width:40%; text-align: center; margin-top: auto; margin-bottom:auto; padding:20vh; padding:5vw "
-      >
-        <v-img
-        class="d-flex"
-          :src="sample.src"
-          style="text-align: center; margin-top: auto; margin-bottom:auto; padding:20vh; padding:5vw "
-          gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-        ></v-img>
+      <div class="row justify-content-between flex" style="margin:auto; width:80%">
+        <div
+          class="col-md-5"
+          style=" text-align: center; margin-top: auto; margin-bottom:auto; padding:20vh; padding:5vw "
+        >
+          <div style="width:80%;" class="center">
+            <v-img
+              src="../../assets/images/sample1.png"
+              style="text-align: center; margin-top:auto; margin-bottom:auto; margin-left:50px; padding:0px "
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            ></v-img>
+          </div>
+        </div>
+        <div
+          class="col-md-5"
+          style="width:40%; text-align:center; margin-top: 0; margin-bottom:auto; padding:20vh; padding:5vw "
+        >
+          <h1>{{tonic.name}}</h1>
+
+          <dl class="product-table">
+            <dt>제조사</dt>
+            <dd>{{ tonic.manufacturer }}</dd>
+            <dt>타입</dt>
+            <dd>{{ tonic.product_form }}</dd>
+          </dl>
+        </div>
       </div>
-      <div class="d-flex" style="width:40%; text-align: center; margin-top: auto; margin-bottom:auto; padding:20vh; padding:5vw " >
-    <h1>상품 정보</h1>
-    <dl class="product-table">
-      <dt>상품 이름</dt>
-      <dd>{{ sample.name }}</dd>
-      <dt>효능</dt>
-      <dd>{{ sample.effect }}</dd>
-      
-    </dl>
-  </div>
-      <!-- <div
-        class="d-flex"
-        style="width:40%; padding:20vh; padding:10vw; margin:0px; "
+      <section
+        style="width: 100vw;
+            height: 100vh;
+            padding: 10px;
+            margin:0 auto;
+            "
       >
-        {{sample.name}}
-        {{ $route.sampletonic_pk }}
-      </div> -->
+        <div style="margin:auto; width:80%">
+          <h2>포함된 영양소</h2>
+          <br />
+          <v-row style="margin-left:20px">
+            <div v-for="(nutrient, i) in tonic.nutrients" :key="i">
+              <p v-if="i==0">{{nutrient.name}}</p>
+              <p v-if="i!=0">, {{nutrient.name}}</p>
+
+              <!-- <v-col v-for="(functional, i) in nutrient.functionals" :key="i" >
+               <p>{{functional.category}}</p>
+               
+              </v-col>-->
+            </div>
+          </v-row>
+        </div>
+        <div style="margin:auto; width:80%">
+          <Review />
+        </div>
+      </section>
     </section>
-    
   </div>
 </template>
 
 <script>
 import simage1 from "../../assets/images/sample1.png";
+import Review from "../../components/post/Review";
+const API_BASE_URL = "http://localhost:8000";
 export default {
-  data: () => ({
-    sample: { name: "아이키커", effect:"뼈 발달에 좋다" ,src: simage1 },
-  }),
+  data() {
+    return {
+      sample: { name: "아이키커", effect: "뼈 발달에 좋다", src: simage1 },
+      tonic: [],
+      params: this.$route.params.tonic_pk,
+      tonicsrc: "",
+    };
+  },
+  components: {
+    Review,
+  },
+  created() {
+    this.fetchTonic();
+    this.tonicsrc = `https://firebasestorage.googleapis.com/v0/b/pillsogood-764c8.appspot.com/o/images%2F${this.tonic.name}_1.jpg?alt=media`;
+  },
+  methods: {
+    fetchTonic() {
+      const API_TONIC_URL = API_BASE_URL + `/supplements/${this.params}`;
+      const config = {};
+      this.$axios
+        .get(API_TONIC_URL, config)
+        .then((res) => {
+          console.log(res.data);
+          this.tonic = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
 };
 </script>
 
