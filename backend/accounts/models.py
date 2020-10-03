316a172d8ef548f92a7c56c5834226df73071330
rsm4282@naver.com <rsm4282@naver.com>
2020-10-03 19:20:15 +0900

    :new: | Feat get recommendation by user functional | S03P23A506-43

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,11 +3,12 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 
-from supplements.models import Functional
+from supplements.models import Category
 
 
 # 사용자 지정 필드 추가하기
 class CustomUser(AbstractUser):
     birth = models.DateField(null=True)
     gender = models.BooleanField(null=True)
-    interests = models.ManyToManyField(Functional, related_name='users', blank=True)
+    interests = models.ManyToManyField(
+        Category, related_name='users', blank=True)
--- backend/recommends/views.py
+++ backend/recommends/views.py
@@ -10,59 +10,58 @@ import pandas as pd
 from django_pandas.io import read_frame
 import numpy as np
 from scipy.sparse.linalg import svds
+from django.http.response import JsonResponse
 # Create your views here.
 
 
 @api_view(['GET'])
-# @permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated])
 def get_recommend_supplementList(request):
-    supplement_by_functional(2)
-    # df_reviews = read_frame(Review.objects.all(), verbose=False)
+    df_reviews = read_frame(Review.objects.all(), verbose=False)
 
-    # # 영양제 데이터프레임
-    # df_supplements = read_frame(Supplement.objects.all(), verbose=False)
+    # 영양제 데이터프레임
+    df_supplements = read_frame(Supplement.objects.all(), verbose=False)
 
-    # # 유저-영양제 평점 데이터프레임
-    # df_reviews = df_reviews[['rank', 'supplement', 'user']]
-    # df_user_supplement_ranks = df_reviews.pivot_table(
-    #     index='user',
-    #     columns='supplement',
-    #     values='rank'
-    # ).fillna(0)
+    # 유저-영양제 평점 데이터프레임
+    df_reviews = df_reviews[['rank', 'supplement', 'user']]
+    df_user_supplement_ranks = df_reviews.pivot_table(
+        index='user',
+        columns='supplement',
+        values='rank'
+    ).fillna(0)
 
-    # matrix = df_user_supplement_ranks.to_numpy()
+    matrix = df_user_supplement_ranks.to_numpy()
 
-    # user_ranks_mean = np.mean(matrix, axis=1)
+    user_ranks_mean = np.mean(matrix, axis=1)
 
-    # # 평점 조정
-    # matrix_user_mean = matrix - user_ranks_mean.reshape(-1, 1)
+    # 평점 조정
+    matrix_user_mean = matrix - user_ranks_mean.reshape(-1, 1)
 
-    # # SVD U,sigma Vt 행렬을 구한다.
-    # U, sigma, Vt = svds(matrix_user_mean, k=2)  # 추후 k 값 수정 필요
+    # SVD U,sigma Vt 행렬을 구한다.
+    U, sigma, Vt = svds(matrix_user_mean, k=2)  # 추후 k 값 수정 필요
 
-    # sigma = np.diag(sigma)
-    # svd_user_predicted_ranks = np.dot(
-    #     np.dot(U, sigma), Vt) + user_ranks_mean.reshape(-1, 1)
+    sigma = np.diag(sigma)
+    svd_user_predicted_ranks = np.dot(
+        np.dot(U, sigma), Vt) + user_ranks_mean.reshape(-1, 1)
 
-    # df_svd_preds = pd.DataFrame(
-    #     svd_user_predicted_ranks,
-    #     index=df_user_supplement_ranks.index,
-    #     columns=df_user_supplement_ranks.columns
-    # )
+    df_svd_preds = pd.DataFrame(
+        svd_user_predicted_ranks,
+        index=df_user_supplement_ranks.index,
+        columns=df_user_supplement_ranks.columns
+    )
 
-    # # 이미 리뷰 작성한 영양제, 협업 필터링 추천 영양제
-    # already_rated, predictions = collaboration_filtering(
-    #     df_svd_preds, request.user.id, df_supplements, df_reviews, 10)
+    # 이미 리뷰 작성한 영양제, 협업 필터링 추천 영양제
+    already_rated, predictions = collaboration_filtering(
+        df_svd_preds, request.user.id, df_supplements, df_reviews, 10)
 
-    # cf_predictions = []
-    # for i, prediction in predictions.iterrows():
-    #     supplement = get_object_or_404(Supplement, pk=prediction['id'])
-    #     cf_predictions.append(supplement)
+    cf_predictions = []
+    for i, prediction in predictions.iterrows():
+        supplement = get_object_or_404(Supplement, pk=prediction['id'])
+        cf_predictions.append(supplement)
 
-    # serializers = SupplementListSerializer(cf_predictions, many=True)
+    serializers = SupplementListSerializer(cf_predictions, many=True)
 
-    # return Response(serializers.data)
-    return Response()
+    return Response(serializers.data)
 
 
 # 리뷰 수가 적으면 추천이 제대로 안됨
@@ -89,6 +88,26 @@ def collaboration_filtering(df_svd_preds, user_id, ori_supplements_df, ori_ranks
         columns={user_row_number: 'Predictions'}).sort_values('Predictions')
 
     return user_history, recommendations
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_recommend_functional(request):
+    data = {}
+    # print(request.user.interests)
+    for interest in request.user.interests.values():
+        res = []
+        recommends = supplement_by_functional(interest['id'])
+        if recommends.empty:
+            continue
+
+        for recommend in recommends.iterrows():
+            print(recommend[1]['id'])
+            supplement = get_object_or_404(Supplement, pk=recommend[1]['id'])
+            res.append(supplement)
+        data[interest['name']] = SupplementListSerializer(res, many=True).data
+    print(data)
+    return Response(data)
 
 
 def supplement_by_functional(category_id, min_reviews=10):
@@ -125,4 +144,4 @@ def supplement_by_functional(category_id, min_reviews=10):
 
     ranks = ranks.sort_values(by=['rank'], ascending=False)
 
-    print(ranks.reset_index())
+    return ranks.head(10).reset_index()
